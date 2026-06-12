7adc79a2b2f61652f6b3f7048ebf16221a4ffe02
Nina Lazarevska <89201209@student.upr.si>
2026-06-13 00:36:56 +0200

    created login page

--- front-end/src/routes/AppRouter.jsx
+++ front-end/src/routes/AppRouter.jsx
@@ -3,18 +3,16 @@ import { BrowserRouter, Routes, Route } from "react-router";
 // import News from "../pages/News";
 // import SingleNews from "../pages/SingleNews";
 // import About from "../pages/About";
-// import Login from "../pages/Login";
+import Login from "../pages/Login";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
-      <Menu />
-
       <Routes>
         {/* <Route path="/news" element={<News />} />
         <Route path="/news/:id" element={<SingleNews />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/login" element={<Login />} /> */}
+        <Route path="/about" element={<About />} /> */}
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
